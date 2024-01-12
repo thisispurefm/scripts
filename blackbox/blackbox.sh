7619e2d476d4c388260e3b55a112c82a409a3c07
Hugh Baldwin <56177385+HughTB@users.noreply.github.com>
2024-01-12 21:24:58 +0000

    feat: logging - issue #1

--- blackbox/blackbox.sh
+++ blackbox/blackbox.sh
@@ -15,6 +15,11 @@ do
     CURRENT_DIR=$SAVE_DIR/$(date +"%Y/%m/%d")
     mkdir -p $CURRENT_DIR
 
+    CURRENT_FILE=$(date +"%H-%M-%S").mp3
+    echo "Start recording file $CURRENT_FILE at $(date +"%Y-%m-%d-%H:%M:%S")" >> $CURRENT_DIR/record.log
+
     # Actually record
-    ffmpeg -f alsa -ac 2 -ar $SAMPLE_RATE -i hw:0 -ab $BITRATE -acodec libmp3lame -t $CHUNK_LENGTH $CURRENT_DIR/$(date +"%H-%M-%S").mp3
+    ffmpeg -f alsa -ac 2 -ar $SAMPLE_RATE -i hw:0 -ab $BITRATE -acodec libmp3lame -t $CHUNK_LENGTH $CURRENT_DIR/$CURRENT_FILE
+
+    echo "Stop recording file $CURRENT_FILE at $(date +"%Y-%m-%d-%H:%M:%S")" >> $CURRENT_DIR/record.log
 done
