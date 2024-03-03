9a70924553e61959153ab1042705ec23615044f4
HughTB <hugh.baldwin77@gmail.com>
2024-03-03 20:42:32 +0000

    update bb script to record on the hour

--- blackbox/blackbox.sh
+++ blackbox/blackbox.sh
@@ -1,25 +1,36 @@
 #!/bin/sh
 
 # File settings
-CHUNK_LENGTH=1800
 SAVE_DIR=/home/puretech/record/
 
 # Audio settings
 SAMPLE_RATE=44100
-BITRATE=96k
+BITRATE=192k
 
-# Infinite loop
-while :
-do
+record()
+{
+    RECORD_LENGTH=$1
+
     # Make the folder for this chunk if it doesn't exist
     CURRENT_DIR=$SAVE_DIR/$(date +"%Y/%m/%d")
     mkdir -p $CURRENT_DIR
 
     CURRENT_FILE=$(date +"%H-%M-%S").mp3
-    echo "Start recording file $CURRENT_FILE at $(date +"%Y-%m-%d-%H:%M:%S")" >> $CURRENT_DIR/record.log
+    echo "Start recording file $CURRENT_FILE at $(date +"%Y-%m-%d-%H:%M:%S") for $RECORD_LENGTH seconds" | tee $CURRENT_DIR/record.log
 
     # Actually record
-    ffmpeg -f alsa -ac 2 -ar $SAMPLE_RATE -i hw:0 -ab $BITRATE -acodec libmp3lame -t $CHUNK_LENGTH $CURRENT_DIR/$CURRENT_FILE
+    ffmpeg -f alsa -ac 2 -ar $SAMPLE_RATE -i hw:0 -ab $BITRATE -acodec libmp3lame -t $RECORD_LENGTH $CURRENT_DIR/$CURRENT_FILE
 
-    echo "Stop recording file $CURRENT_FILE at $(date +"%Y-%m-%d-%H:%M:%S")" >> $CURRENT_DIR/record.log
+    echo "Stop recording file $CURRENT_FILE at $(date +"%Y-%m-%d-%H:%M:%S")" | tee $CURRENT_DIR/record.log
+}
+
+echo "SCRIPT STARTING"
+
+# Infinite loop
+while :
+do
+    NEXT_HOUR=$(( $(date +%s -d "$(date +%H -d 'hour'):00") - $(date +%s) ))
+    record $NEXT_HOUR
 done
+
+echo "SCRIPT EXITED"
