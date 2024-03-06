e296bd238f56472956bb3a9520272a7d1e6d6a68
Thomas Boxall <38958967+ThomasBoxall@users.noreply.github.com>
2024-03-06 17:30:51 +0000

    screaming, crying and throwing up

--- blackbox/blackbox.sh
+++ blackbox/blackbox.sh
@@ -13,18 +13,24 @@ record()
 
     # Make the folder for this chunk if it doesn't exist
     CURRENT_DIR=$SAVE_DIR/$(date +"%Y/%m/%d")
-    # mkdir -p $CURRENT_DIR
+    mkdir -p $CURRENT_DIR
 
     CURRENT_FILE=$(date +"%H-%M-%S").mp3
-    echo "Start recording file $CURRENT_FILE at $(date +"%Y-%m-%d-%H:%M:%S") for $RECORD_LENGTH seconds" | tee $CURRENT_DIR/record.log
+    echo "Start recording file $CURRENT_FILE at $(date +"%Y-%m-%d-%H:%M:%S") for $RECORD_LENGTH seconds" | tee -a $CURRENT_DIR/record.log
 
     # Actually record
-    # ffmpeg -f alsa -ac 2 -ar $SAMPLE_RATE -i hw:0 -ab $BITRATE -acodec libmp3lame -t $RECORD_LENGTH $CURRENT_DIR/$CURRENT_FILE
+    ffmpeg -f alsa -ac 2 -ar $SAMPLE_RATE -i hw:0 -ab $BITRATE -acodec libmp3lame -t $RECORD_LENGTH $CURRENT_DIR/$CURRENT_FILE
 
-    echo "Stop recording file $CURRENT_FILE at $(date +"%Y-%m-%d-%H:%M:%S")" | tee $CURRENT_DIR/record.log
+    echo "Stop recording file $CURRENT_FILE at $(date +"%Y-%m-%d-%H:%M:%S")" | tee -a $CURRENT_DIR/record.log
 }
 
 echo "SCRIPT STARTING"
+
+echo "Setting volumes"
+
+amixer -c 0 set "Front Mic" 0db
+amixer -c 0 set "Front Mic Boost" 0db
+amixer -c 0 set "Capture" 0db
 
 # Infinite loop
 while :
